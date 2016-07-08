579ec3074c9aeb8ac4f0c175201f0ec9960287d4
robin <chou_robin@hotmail.com>
2016-07-08 14:43:57 +0800

    modify

--- music.py
+++ music.py
@@ -10,14 +10,15 @@ import urllib2
 import urllib
 import re
 
-mv_id = '2275893'   # 这里输入mv的id，即http://v.yinyuetai.com/video/2275893最后的数字
+#mv_id = '2275893'   # 这里输入mv的id，即http://v.yinyuetai.com/video/2275893最后的数字
+
+mv_id =str(input("mv id:"))
 
 url = "http://www.yinyuetai.com/insite/get-video-info?flex=true&videoId=" + mv_id 
 timeout = 30
 headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-
 }
 
 req = urllib2.Request(url, None, headers)
@@ -26,15 +27,15 @@ html = res.read()
 
 reg = r"http://\w*?\.yinyuetai\.com/uploads/videos/common/.*?(?=&br)"
 pattern=re.compile(reg)
-findList = re.findall(pattern,html)     # 找到mv所有版本的下载链接
+findList = re.findall(pattern,html)                                     # 找到mv所有版本的下载链接
 
 if len(findList) >= 3: 
     print '>=3'
-    mvurl = findList[2]                 # 含有流畅、高清、超清三个版本时下载超清
+    mvurl = findList[2]                                                 # 含有流畅、高清、超清三个版本时下载超清
 else:
-    mvurl = findList[0]                 # 版本少时下载流畅视频
+    mvurl = findList[0]                                                 # 版本少时下载流畅视频
     
-local = 'MV.flv'
+local = mv_id + 'MV.flv'
 
 try:
     print 'downloading...please wait...'
